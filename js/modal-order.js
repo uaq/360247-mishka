a0254ad7ca63c50e8170012a3387305a7d8ee03e
Eyespot <eger_np_@inbox.ru>
2017-06-22 09:37:59 +0300

    build

---
+++ js/modal-order.js
@@ -0,0 +1,38 @@
+var link = document.querySelector(".order-btn");
+
+var popup = document.querySelector(".modal-order");
+var close = popup.querySelector(".modal-order__form-close");
+//var radio = popup.querySelector("");
+var form = popup.querySelector(".modal-order__form");
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-order__show");
+});
+
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-order__show");
+//    popup.classList.remove("modal-form-error");
+});
+
+//form.addEventListener("submit", function(event) {
+//    if (!nameinput.value || !emailinput.value || !letterarea.value) {
+//        event.preventDefault();
+//        popup.classList.remove("modal-form-error");
+//        popup.offsetWidth = popup.offsetWidth;
+//        popup.classList.add("modal-form-error");
+//    } else {
+//        localStorage.setItem("nameinput", nameinput.value);
+//        localStorage.setItem("emailinput", emailinput.value);
+//    }
+//});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-order__show")) {
+            popup.classList.remove("modal-order__show");
+//            popup.classList.remove("modal-form-error");
+        }
+    }
+});
